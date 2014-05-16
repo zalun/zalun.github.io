b331ec8cc1792949ea745224a21ad10f75b15661
Piotr Zalewa <piotr@zalewa.info>
2014-05-16 10:53:01 +0200

    source separated for techy 1.0 still not perfect

---
+++ src/lib/list.techy.js
@@ -0,0 +1,18 @@
+module.exports = function(template, max) {
+	template = template || 'partials/article-home.md';
+  template = 'src/' + template;
+	var articles = this.pages('articles', 'date');
+	var html = '', 
+      to = Math.min(max, articles.length) || articles.length;
+	for(var i=0; i<to; i++) {
+		var article = articles[i];
+    html += this.template(template, {
+      title: article.get('title'),
+      preface: article.get('preface'),
+      link: this.linkto(article),
+      date: article.get('date'),
+      tags: article.get('tags').join(', ')
+    });
+	}
+	return html;
+}
